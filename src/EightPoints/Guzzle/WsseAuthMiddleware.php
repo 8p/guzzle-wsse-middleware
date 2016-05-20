2916f1c048f612b81e13226d55563e316b7fa885
Florian Preusner <florian.preusner@project-a.com>
2016-05-20 20:44:29 +0200

    PSR2 standard

--- src/EightPoints/Guzzle/WsseAuthMiddleware.php
+++ src/EightPoints/Guzzle/WsseAuthMiddleware.php
@@ -2,21 +2,16 @@
 
 namespace EightPoints\Guzzle;
 
-use       Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\RequestInterface;
 
 /**
  * Adds WSSE auth headers based on http://www.xml.com/pub/a/2003/12/17/dive.html
  *
- * @package   EightPoints\Guzzle
- *
- * @copyright 8points IT
- * @author    Florian Preusner
- *
  * @version   2.0
  * @since     2013-10
  */
-class WsseAuthMiddleware {
-
+class WsseAuthMiddleware
+{
     /**
      * @var string $username
      */
@@ -28,89 +23,75 @@ class WsseAuthMiddleware {
     private $password;
 
     /**
-     * Constructor
-     *
-     * @author  Florian Preusner
      * @version 1.0
      * @since   2013-10
      *
-     * @param   string $username
-     * @param   string $password
+     * @param string $username
+     * @param string $password
      */
-    public function __construct($username, $password) {
-
-        $this->setUsername( $username);
-        $this->setPassword( $password);
-    } // end: __construct()
+    public function __construct($username, $password)
+    {
+        $this->setUsername($username);
+        $this->setPassword($password);
+    }
 
     /**
-     * Get Username
-     *
-     * @author  Florian Preusner
      * @version 1.0
      * @since   2013-10
      *
      * @return  string $username
      */
-    public function getUsername() {
-
+    public function getUsername()
+    {
         return $this->username;
-    } // end: getUsername()
+    }
 
     /**
-     * Set Username
-     *
-     * @author  Florian Preusner
      * @version 1.0
      * @since   2013-10
      *
-     * @param   string $value
-     * @return  void
+     * @param  string $value
+     * @return void
      */
-    public function setUsername($value) {
-
+    public function setUsername($value)
+    {
         $this->username = $value;
-    } // end: setUsername()
+    }
 
     /**
-     * Get Password
-     *
-     * @author  Florian Preusner
      * @version 1.0
      * @since   2013-10
      *
-     * @return  string $password
+     * @return string $password
      */
-    public function getPassword() {
-
+    public function getPassword()
+    {
         return $this->password;
-    } // end: getPassword()
+    }
 
     /**
-     * Set Password
-     *
-     * @author  Florian Preusner
      * @version 1.0
      * @since   2013-10
      *
-     * @param   string $value
-     * @return  void
+     * @param  string $value
+     * @return void
      */
-    public function setPassword($value) {
-
+    public function setPassword($value)
+    {
         $this->password = $value;
-    } // end: setPassword()
+    }
 
     /**
      * Add WSSE auth headers to Request
      *
-     * @author  Florian Preusner
      * @version 3.0
      * @since   2015-06
      *
-     * @return  callable
+     * @return callable
+     * @throws \InvalidArgumentException
      */
-    public function attach() {
+    public function attach()
+    {
         return function (callable $handler) {
 
             return function (RequestInterface $request, array $options) use ($handler) {
@@ -119,10 +100,10 @@ class WsseAuthMiddleware {
                 $digest = $this->generateDigest($nonce, $createdAt, $this->password);
 
                 $xwsse = array(
-                    sprintf('Username="%s"',       $this->username),
+                    sprintf('Username="%s"', $this->username),
                     sprintf('PasswordDigest="%s"', $digest),
-                    sprintf('Nonce="%s"',          $nonce),
-                    sprintf('Created="%s"',        $createdAt)
+                    sprintf('Nonce="%s"', $nonce),
+                    sprintf('Created="%s"', $createdAt)
                 );
 
                 $request = $request->withHeader('Authorization', 'WSSE profile="UsernameToken"');
@@ -131,37 +112,33 @@ class WsseAuthMiddleware {
                 return $handler($request, $options);
             };
         };
-    } // end: attach()
+    }
 
     /**
-     * Generate Digest
-     *
-     * @author  Florian Preusner
      * @version 1.0
      * @since   2013-10
      *
-     * @param   string $nonce
-     * @param   string $createdAt
-     * @param   string $password
+     * @param string $nonce
+     * @param string $createdAt
+     * @param string $password
      *
-     * @return  string
+     * @return string
      */
-    public function generateDigest($nonce, $createdAt, $password) {
-
+    public function generateDigest($nonce, $createdAt, $password)
+    {
         return base64_encode(sha1(base64_decode($nonce) . $createdAt . $password, true));
-    } // end: generateDigest()
+    }
 
     /**
      * Generate Nonce (number user once)
      *
-     * @author  Florian Preusner
      * @version 1.0
      * @since   2013-10
      *
-     * @return  string
+     * @return string
      */
-    public function generateNonce() {
-
+    public function generateNonce()
+    {
         return base64_encode(hash('sha512', uniqid(true)));
-    } // end: generateNonce()
-} // end: WsseAuthMiddleware
+    }
+}
